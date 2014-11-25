641de65a2fddaa3939fd0347cf9b1eea3e0834f2
Liam Dunne <liamodunne@mac.com>
2014-11-25 12:07:16 +0000

    Renamed original script to _solo, updated the main script to allow for multiple character takes (based on script parameters) eg.
    Calling the script with 3 arguments:
    > too_many_cooks.sc "Smarf" "Lars Von Trier (as “Pie”)" "Katie Adkins"
    
    This will show the following prompts, requesting keyboard input before taking the photo sequences:
    > Get Smarf to sit in front of the camera, press any key when ready:
    > Get Lars Von Trier (as “Pie”) to sit in front of the camera, press any key when ready:
    > Get Katie Adkins to sit in front of the camera, press any key when ready:
    
    In each case, the webcam will start up, the computer will beep when the photos are starting and ending (2.5 seconds worth)

--- too-many-cooks.sh
+++ too-many-cooks.sh
@@ -1,85 +1,111 @@
-# pre-requirements:
-# brew get imagesnap
-# brew get imagemagick
-
-if [[ ("$#" == 0) ]]; then 
-    echo 'Usage: too_many_cooks.sc "caption"'
-    return
-fi
-
-CAPTION="$@"
-
 # pre-cleanup
 FILEMASK=*snapshot-*.jpg
 rm -rf $FILEMASK
 
-# take photos for 2 seconds after a 1 second delay
-(sleep 1; printf "\a") &
-/usr/local/bin/imagesnap -t 0.1 -w 1 photo.png & sleep 4 ; kill $!
-printf "\a"
+#parse through arguments
+args=$@
+CAPTIONS=( "$@" )
+CAPTIONCOUNT=${#CAPTIONS[@]}
 
-#get last file taken
-FILEMASK=*snapshot-*.jpg
-INTRO_FILEMASK=intro/Too_Many_Cooks_Intro*.png
-FILES=(${FILEMASK// // })
-RENDERED_FILENAME_PREFIX=rendered
-LAST_FILE=${RENDERED_FILENAME_PREFIX}_${FILES[${#FILES[@]} - 1]}
+for index in "${!CAPTIONS[@]}"; do
+	#get caption
+	CAPTION=${CAPTIONS[$index]}
+	#strip spaces
+	CAPTION_NOSPACES=${CAPTION// /.}
 
-# resize files
-FILECOUNT=${#FILES[@]}
-THRESHOLD=$(($FILECOUNT/4))
-echo THRESHOLD \/ FILECOUNT = $THRESHOLD \/ $FILECOUNT
+	read -p "Get $CAPTION to sit in front of the camera, press any key when ready:" -n1 -s
+	echo
 
-for (( i = 0 ; i < $FILECOUNT ; i++ )) 
-do
-	FILE=${FILES[$i]}
-	RESIZED_FILENAME=${FILE}_resized.jpg
+	#take photos
+	(sleep 1; printf "\a") &
+	/usr/local/bin/imagesnap -q -t 0.1 -w 1.0 photo.png & sleep 3.5 ; kill $!
+	printf "\a"
 
-	# resize file
-	convert $FILE -resize 320x320\> -size 320x $RESIZED_FILENAME
+	#reset filemask to just the last batch of imagesnaps
+	FILEMASK=snapshot-*.jpg
 
-	if [ $i -lt $THRESHOLD ] ; then
-		font_caption="Palatino-BoldItalic"
-		fontsize_caption=18
+	#rename photos
+	for FILE in $FILEMASK;  do 
+		RENAMEDFILE=$(printf %04d_%s_%s.jpg ${index} ${CAPTION_NOSPACES} ${FILE%.jpg})
+		mv $FILE $RENAMEDFILE
+	done
 
-		convert $RESIZED_FILENAME \
-			-gravity center \
-			-font "$font_caption" -pointsize $fontsize_caption \
-			-stroke '#000C' -strokewidth 2 -annotate +0+20 "Starring" \
-			-stroke  none   -fill yellow   -annotate +0+20 "Starring" \
-			-level 0%,100%,1.2 \
-			-define dpx:television.time.code=10:00:02:15 \
-			-auto-level \
-			"${RENDERED_FILENAME_PREFIX}"_"${FILE}"
+done
 
-	else
-		font_caption="Palatino-BoldItalic"
-		fontsize_caption=28
+for index in "${!CAPTIONS[@]}"; do
 
-		convert $RESIZED_FILENAME \
-			-gravity south \
-			-font "$font_caption" -pointsize $fontsize_caption \
-			-stroke '#000C' -strokewidth 2 -annotate +0+20 "${CAPTION}" \
-			-stroke  none   -fill yellow   -annotate +0+20 "${CAPTION}" \
-			-level 0%,100%,1.2 \
-			-define dpx:television.time.code=10:00:02:15 \
-			-auto-level \
-			"${RENDERED_FILENAME_PREFIX}"_"${FILE}"
+	#get current caption
+	CAPTION=${CAPTIONS[$index]}
+	#strip spaces form current caption
+	CAPTION_NOSPACES=${CAPTION// /.}
+
+	#filemask for current caption
+	FILEMASK=$(printf %04d_%s_*.jpg ${index} ${CAPTION_NOSPACES})
+
+	#step through all files for current caption
+	FILES=(${FILEMASK// // })
+
+	TITLE="Starring"
+	if [ $index -ne 0 ] ; then
+		TITLE="Also starring"
 	fi
 
-	rm -rf $FILE
+	FILECOUNT=${#FILES[@]}
+	THRESHOLD=$(($FILECOUNT/4))
+
+	for fileIndex in "${!FILES[@]}"; do
+
+		FILE=${FILES[$fileIndex]}
+
+		# resize file
+		RESIZED_FILE=$(printf %s_resized.jpg ${FILE%.jpg})
+		convert $FILE -resize 320x320\> -size 320x $RESIZED_FILE
+
+		CAPTIONED_FILE=$(printf %s_captioned.jpg ${FILE%_resized.jpg})
+		rm -rf $FILE
+
+		if [ $fileIndex -lt $THRESHOLD ] ; then
+			font_caption="Palatino-BoldItalic"
+			fontsize_caption=18
+
+			convert $RESIZED_FILE \
+				-gravity center \
+				-font "$font_caption" -pointsize $fontsize_caption \
+				-stroke '#000C' -strokewidth 2 -annotate +0+20 "${TITLE}" \
+				-stroke  none   -fill yellow   -annotate +0+20 "${TITLE}" \
+				-level 0%,100%,1.2 \
+				-define dpx:television.time.code=10:00:02:15 \
+				-auto-level \
+				$CAPTIONED_FILE
+
+		else
+			font_caption="Palatino-BoldItalic"
+			fontsize_caption=28
+
+			convert $RESIZED_FILE \
+				-gravity south \
+				-font "$font_caption" -pointsize $fontsize_caption \
+				-stroke '#000C' -strokewidth 2 -annotate +0+20 "${CAPTION}" \
+				-stroke  none   -fill yellow   -annotate +0+20 "${CAPTION}" \
+				-level 0%,100%,1.2 \
+				-define dpx:television.time.code=10:00:02:15 \
+				-auto-level \
+				$CAPTIONED_FILE
+		fi
+
+		rm -rf $RESIZED_FILE
+
+	done
+
 done
+
+INTRO_FILEMASK=Too_Many_Cooks_Intro*.png
+INTRO_FILEMASK=intro/Too_Many_Cooks_Intro*.png
+CAPTIONED_FILEMASK=*_captioned.jpg
 
 # convert file sequence to animated gif
 GIF_FILENAME=Too_Many_Cooks_-$(date +%y%m%d%H%M%S).gif
-# convert -delay 10 "${RENDERED_FILENAME_PREFIX}"*.jpg -delay 100 $LAST_FILE -delay 10 $GIF_FILENAME
-convert -delay 12 $INTRO_FILEMASK "${RENDERED_FILENAME_PREFIX}"*.jpg -delay 120 $LAST_FILE -ordered-dither o8x8,8,8,4 -layers OptimizeTransparency $GIF_FILENAME
 
-# quicklook gif
-qlmanage -p $GIF_FILENAME >& /dev/null
+convert -delay 12 $INTRO_FILEMASK $CAPTIONED_FILEMASK -ordered-dither o8x8,8,8,4 -layers OptimizeTransparency $GIF_FILENAME
 
-# copy path to gif
-echo `pwd`/$GIF_FILENAME | pbcopy
-
-# post-cleanup
-rm -rf $FILEMASK
+rm -rf *jpg
---
+++ too-many-cooks_solo.sh
@@ -0,0 +1,85 @@
+# pre-requirements:
+# brew get imagesnap
+# brew get imagemagick
+
+if [[ ("$#" == 0) ]]; then 
+    echo 'Usage: too_many_cooks.sc "caption"'
+    return
+fi
+
+CAPTION="$@"
+
+# pre-cleanup
+FILEMASK=*snapshot-*.jpg
+rm -rf $FILEMASK
+
+# take photos for 2 seconds after a 1 second delay
+(sleep 1; printf "\a") &
+/usr/local/bin/imagesnap -t 0.1 -w 1 photo.png & sleep 4 ; kill $!
+printf "\a"
+
+#get last file taken
+INTRO_FILEMASK=intro/Too_Many_Cooks_Intro*.png
+FILEMASK=*snapshot-*.jpg
+FILES=(${FILEMASK// // })
+RENDERED_FILENAME_PREFIX=rendered
+LAST_FILE=${RENDERED_FILENAME_PREFIX}_${FILES[${#FILES[@]} - 1]}
+
+# resize files
+FILECOUNT=${#FILES[@]}
+THRESHOLD=$(($FILECOUNT/4))
+echo THRESHOLD \/ FILECOUNT = $THRESHOLD \/ $FILECOUNT
+
+for (( i = 0 ; i < $FILECOUNT ; i++ )) 
+do
+	FILE=${FILES[$i]}
+	RESIZED_FILENAME=${FILE}_resized.jpg
+
+	# resize file
+	convert $FILE -resize 320x320\> -size 320x $RESIZED_FILENAME
+
+	if [ $i -lt $THRESHOLD ] ; then
+		font_caption="Palatino-BoldItalic"
+		fontsize_caption=18
+
+		convert $RESIZED_FILENAME \
+			-gravity center \
+			-font "$font_caption" -pointsize $fontsize_caption \
+			-stroke '#000C' -strokewidth 2 -annotate +0+20 "Starring" \
+			-stroke  none   -fill yellow   -annotate +0+20 "Starring" \
+			-level 0%,100%,1.2 \
+			-define dpx:television.time.code=10:00:02:15 \
+			-auto-level \
+			"${RENDERED_FILENAME_PREFIX}"_"${FILE}"
+
+	else
+		font_caption="Palatino-BoldItalic"
+		fontsize_caption=28
+
+		convert $RESIZED_FILENAME \
+			-gravity south \
+			-font "$font_caption" -pointsize $fontsize_caption \
+			-stroke '#000C' -strokewidth 2 -annotate +0+20 "${CAPTION}" \
+			-stroke  none   -fill yellow   -annotate +0+20 "${CAPTION}" \
+			-level 0%,100%,1.2 \
+			-define dpx:television.time.code=10:00:02:15 \
+			-auto-level \
+			"${RENDERED_FILENAME_PREFIX}"_"${FILE}"
+	fi
+
+	rm -rf $FILE
+done
+
+# convert file sequence to animated gif
+GIF_FILENAME=Too_Many_Cooks_-$(date +%y%m%d%H%M%S).gif
+# convert -delay 10 "${RENDERED_FILENAME_PREFIX}"*.jpg -delay 100 $LAST_FILE -delay 10 $GIF_FILENAME
+convert -delay 12 $INTRO_FILEMASK "${RENDERED_FILENAME_PREFIX}"*.jpg -delay 120 $LAST_FILE -ordered-dither o8x8,8,8,4 -layers OptimizeTransparency $GIF_FILENAME
+
+# quicklook gif
+qlmanage -p $GIF_FILENAME >& /dev/null
+
+# copy path to gif
+echo `pwd`/$GIF_FILENAME | pbcopy
+
+# post-cleanup
+rm -rf $FILEMASK
